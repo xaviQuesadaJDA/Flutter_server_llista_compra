c7c9073b08537c314286e4cdb515bf47215b8f8d
Xavi <profeciclesformatius@gmail.com>
2023-11-22 12:58:48 +0100

    using api-keys

--- llista_compra.py
+++ llista_compra.py
@@ -4,15 +4,15 @@ from flask import Flask, request, jsonify
 import uuid
 import hashlib
 import mysql.connector
-from dotenv import load_dotenv
-import os
+import json
 
-load_dotenv()
 app = Flask(__name__)
 
-
+valid_api_keys = {}
 articles = []
 last_id = 0
+with open("env.json") as conf:
+  env = json.load(conf)
 
 users = {'obret':hashlib.sha256(b"sesam").hexdigest(), 'xavi':hashlib.sha256(b"1234").hexdigest()}
 def check_tables():
@@ -33,7 +33,7 @@ def create_tables():
   cursor.execute("Drop table if exists articles;")
   cursor.execute("Drop table if exists usuaris;")
   cursor.execute("Drop table if exists api_keys;")
-  
+
   cursor.execute("""
               Create table if not exists articles(
                 id int not null auto_increment,
@@ -62,19 +62,25 @@ def create_tables():
   conn.commit()
 
 conn = mysql.connector.connect(
-                host=os.getenv('DB_HOST'),
-                user=os.getenv('DB_USER'),
-                password=os.getenv('DB_PWD'),
-                database=os.getenv('DB_NAME')
+                host=env['db_host'],
+                user=env['db_user'],
+                password=env['db_pwd'],
+                database=env['db_name']
                 )
 if not check_tables():
     create_tables()
-    
+
 @app.route('/articles/<int:article_id>',
            methods=['GET', 'PUT', 'DELETE'])
 def article(article_id=0):
   global last_id
   global articles
+  global valid_api_keys
+
+  if not "x-api-key" in request.headers:
+      return jsonify({"status": "error", "description": "Falta capçalera x-api-key"}), 401
+  if valid_api_keys.get(request.headers['x-api-key'], None) is None:
+      return jsonify({"status": "error", "description": "Credencials no vàlides"}), 401
   if request.method == 'GET':
     trobat = [art for art in articles if art["id"] == article_id]
     if len(trobat) > 0:
@@ -96,6 +102,12 @@ def article(article_id=0):
 def get_articles():
   global articles
   global last_id
+  global valid_api_keys
+
+  if not "x-api-key" in request.headers:
+      return jsonify({"status": "error", "description": "Falta capçalera x-api-key"}), 401
+  if valid_api_keys.get(request.headers['x-api-key'], None) is None:
+      return jsonify({"status": "error", "description": "Credencials no vàlides"}), 401
   if request.method == 'GET':
     return jsonify(articles)
   elif request.method == 'POST':
@@ -107,7 +119,7 @@ def get_articles():
     }
     articles.append(article)
     return jsonify(article), 201
-  
+
 @app.route('/login', methods=['POST'])
 def login():
   if not "authorization" in request.headers:
@@ -116,8 +128,10 @@ def login():
   usuari = auth.username
   paraula_pas = auth.password
   if users.get(usuari, None) == hashlib.sha256(paraula_pas.encode()).hexdigest():
-    return jsonify({'resultat': 'success', 'x-api-key': uuid.uuid4().hex})
+    nova_api_key = uuid.uuid4().hex
+    valid_api_keys[nova_api_key]= {"user": usuari}
+    print(valid_api_keys)
+    return jsonify({'resultat': 'success', 'x-api-key': nova_api_key})
   return jsonify({'resultat': 'error', 'user':usuari, 'pwd': paraula_pas}), 401
 
-if __name__=="__main__":
-  app.run(host='0.0.0.0')
+app.run()
